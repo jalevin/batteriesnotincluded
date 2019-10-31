4dfeae926216be06d7e13e7104cf69c8729ba062
Jeff Levin <jeff@levinology.com>
2019-10-31 13:28:26 +1100

    basic mux done

--- 4_mux/main.go
+++ 4_mux/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,19 +13,40 @@ import (
 type App struct{}
 
 type Post struct {
+	Id    string `json:"id"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
 
+func findPostById(id string) Post {
+	for _, p := range posts {
+		if p.Id == id {
+			return p
+		}
+	}
+	return Post{}
+}
+
 var posts = []Post{
 	{
+		Id:    "1",
 		Title: "Hey Gophercon!",
 		Body:  "Welcome to Alaska!",
 	}, {
+		Id:    "2",
 		Title: "It's really warm in Australia",
 		Body:  "This is the first day I've worn pants",
 	},
 }
+
+type PageData struct {
+	PageTitle string
+	Flash     string
+	Post
+	Posts []Post
+}
+
+/////Utility functions ///////////////
 
 // Get root directory + folder
 func projectDir() string {
@@ -32,78 +54,119 @@ func projectDir() string {
 	return path.Dir(filename)
 }
 
+// Get static path to template file
 func projectView(view string) string {
 	return path.Join(projectDir(), "views", view+".html")
 }
 
+// Build templates for us
+func buildView(view string) *template.Template {
+	tmpl := template.Must(template.ParseFiles(
+		projectView("layout"),
+		projectView("nav"),
+		projectView(view),
+	))
+
+	return tmpl
+}
+
+//////End Utility Functions ///////////////
+
 // Handler to root path
-func (a App) Root(res http.ResponseWriter, req *http.Request) {
+func (a App) Root(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	http.Redirect(res, req, "/posts", http.StatusFound)
 }
 
 // Handler to show all posts
-func (a App) Posts(res http.ResponseWriter, req *http.Request) {
+func (a App) Posts(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 
-	tmpl := template.Must(template.ParseFiles(
-		projectView("layout"),
-		projectView("nav"),
-		projectView("posts"),
-	))
-
-	err := tmpl.ExecuteTemplate(res, "layout", posts)
-	if err != nil {
-		http.Error(res, err.Error(), http.StatusInternalServerError)
+	// easier to do
+	tmpl := buildView("posts")
+	pd := PageData{
+		PageTitle: "Hello Gophercon!",
+		Posts:     posts,
 	}
-}
 
-// Handler to show a post
-func (a App) ShowPost(res http.ResponseWriter, req *http.Request) {
-
-	tmpl := template.Must(template.ParseFiles(
-		projectView("layout"),
-		projectView("nav"),
-		projectView("show_post"),
-	))
-
-	err := tmpl.ExecuteTemplate(res, "layout", posts[1])
+	// easier to understand what's going on??
+	err := tmpl.ExecuteTemplate(res, "layout", pd)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 // Handler to new post form
-func (a App) NewPost(res http.ResponseWriter, req *http.Request) {
-	tmpl := template.Must(template.ParseFiles(
-		projectView("layout"),
-		projectView("nav"),
-		projectView("new_post"),
-	))
+func (a App) NewPost(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 
-	err := tmpl.ExecuteTemplate(res, "layout", posts)
+	tmpl := buildView("new_post")
+
+	pd := PageData{
+		PageTitle: "New Post",
+		Post:      Post{},
+	}
+
+	err := tmpl.ExecuteTemplate(res, "layout", pd)
 
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+// Handler to show a post
+func (a App) ShowPost(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+
+	tmpl := buildView("show_post")
+
+	// Standard server error, but don't crash
+	p := findPostById(params.ByName("id"))
+	if p.Id == "" {
+		msg := fmt.Sprintf("No post with id %v", params.ByName("id"))
+		http.Error(res, msg, http.StatusInternalServerError)
+	}
+
+	pd := PageData{
+		PageTitle: p.Title,
+		Post:      p,
+	}
+
+	err := tmpl.ExecuteTemplate(res, "layout", pd)
+
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Handler to new post form
+func (a App) CreatePost(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+
+	p := Post{
+		Id:    "3",
+		Title: req.PostFormValue("title"),
+		Body:  req.PostFormValue("body"),
+	}
+
+	posts = append(posts, p)
+
+	http.Redirect(res, req, "/post/3", http.StatusFound)
+}
+
 func main() {
 	var a App
 
-	// Serve static asset
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	router := httprouter.New()
+	router.ServeFiles("/assets/*filepath", http.Dir(path.Join(projectDir(), "assets")))
+	router.GET("/", a.Root)
 
-	// B. Root path
-	http.HandleFunc("/", a.Root)
+	router.GET("/posts", a.Posts)
+	router.GET("/posts/new", a.NewPost)
+	router.POST("/posts/create", a.CreatePost)
 
-	// 1. Posts Path
-	http.HandleFunc("/posts", a.Posts)
-
-	// 2. View
-	http.HandleFunc("/post/1", a.ShowPost)
-
-	// 3. New Post
-	http.HandleFunc("/posts/new", a.NewPost)
+	router.GET("/post/:id", a.ShowPost)
+	//router.GET("/post/:id/edit", a.ShowPost)
 
 	fmt.Println("Listening on localhost:8080...")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jalevin/batteriesnotincluded
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
